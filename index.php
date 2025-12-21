72334d7429d93646c4aa9f2b9cd8e62dae23582c
richardios275 <kaylabridget1112@proton.me>
2025-12-21 13:53:26 +0700

    Removed Vestigial codo

--- index.php
+++ index.php
@@ -1,15 +1,8 @@
 <?php
 session_start();
 if (!isset($_SESSION["logged_in"]) || $_SESSION["logged_in"] !== true) {
-    header("Location: login.php");
+    header("Location: login");
 } else {
     header("Location: home2.php");
-}
-
-$page = $_GET['page'] ?? 'home';
-$pagePath = "pages/$page.php";
-
-if (!file_exists($pagePath)) {
-    $pagePath = "pages/404.php";
 }
 ?>
